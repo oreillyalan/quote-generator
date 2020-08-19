4c309e51e6fe476371f1b3928f22d2487e0a4170
oreillyalan <oreillyalan132@gmail.com>
2020-08-19 15:27:34 +0100

    used my own CORS proxy to fix 429 error

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+const quoteContainer = document.getElementById('quote-contaner');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 // Get Quote From API
 
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const proxyUrl = 'https://nameless-meadow-30358.herokuapp.com/'
     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json()
-        console.log(data)
+        authorText.innerText = data.quoteAuthor
+        quoteText.innerText = data.quoteText
     } catch (error)
     {
         getQuote();
-        console.log('whoops, no quote', error);
     }
 }
 
